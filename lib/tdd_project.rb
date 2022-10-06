48e0c405e91834bace36670beeba1e2ea03d4d99
Daniel <danieltkalla@gmail.com>
2022-10-06 13:54:34 -0400

    too easy, its a problem

--- lib/tdd_project.rb
+++ lib/tdd_project.rb
@@ -2,7 +2,18 @@ class Array
     def my_uniq
         count = Hash.new(0)
         self.each {|ele| count[ele] += 1}
-        self.select{|ele| count[ele] == 1}
+        selected = count.keys
+    end
 
+    def two_sum
+        new_arr = []
+        self.each_with_index do |ele,idx|
+            self.each_with_index do |el,index|
+                if index > idx && ele + el == 0
+                    new_arr << [idx,index]
+                end
+            end
+        end
+        new_arr
     end
 end
--- spec/tdd_project_spec.rb
+++ spec/tdd_project_spec.rb
@@ -1,6 +1,15 @@
+require 'tdd_project'
 
 describe "#my_uniq" do 
- it "removes repeated elements" do
-    expect([1,2,1,3,3].uniq).to eq([1,2,3])
- end
+   it "removes repeated elements" do
+      expect([1,2,1,3,3].my_uniq).to eq([1,2,3])
+   end
 end
+
+describe "#two_sum" do
+   it "find pairs of elements that sum to zero" do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+   end
+end
+
+describe 
